73da33b6604c12edf064ae61341be348e251c598
Aaron Fletcher <aaronfs07@hotmail.com>
2021-06-22 22:25:56 +0200

    Updated the script with some additions to work with continuum. Test ver.

--- namechangeutil-domain.ps1
+++ namechangeutil-domain.ps1
@@ -1,33 +1,24 @@
-#Written by Aaron Fletcher#
+##Written by Aaron Fletcher##
 
-#This script will ingest a list of computers from a csv and import their old names and what the new name is suppsoed to be#
+This will ingest the CSV file that is also attached here as well.
 
-$welcomemsg = "Welcome to the Domain Windows computer renaming tool.
-Please ensure you have Domain Administrator credentials in order to run this tool
-##Run at your own risk. I am not responsible for you messing things up!##"
+$CSVFile = 'c:\scriptComputerList.csv'
 
-Write-Host $welcomemsg -ForegroundColor Yellow
+##This will request domain credentials if using a domain env. If this is a local machine you need to use that.
 
+$Credential=Get-Credential
 
-##Ingest Data from CSV##
+##This will begin the import of the CSV and complete name changes##
 
-$csvmsg = "Beginning import of CSV. If you are not seeing data populate below please look into CSV"
+$computernames= Import-Csv -Path $CSVFile `
+                          -Delimiter "," `
+                          -Header OldName,NewName
 
-Write-Host $csvmsg -ForegroundColor Yellow
-
-$csv = Import-CSV ./computerrename-domain.csv
-$csv | select-object -ExpandProperty old-names -outvariable oldnames
-$csv | select-object -ExpandProperty new-names -outvariable newnames
-
-##Verify remote computers are online and functioning##
-
-
-
-##Rename PCs to given names##
-
-
-##Restart machine afterwards##
-
-
-
-##Reconnect & verify connection/hostname##
+foreach ($Computer in $computernames)
+{
+ Rename-Computer -ComputerName $Computer.OldName `
+                 -NewName $Computer.NewName `
+                 -DomainCredential $Credential `
+                 -Force `
+                 -Restart
+}
